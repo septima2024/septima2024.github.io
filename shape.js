5a392560953968a6f22f58294d85f2bc5ce1bb98
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-04-07 20:16:42 +0200

    Generate and move shapes, and check if shapes can be moved

--- shape.js
+++ shape.js
@@ -1,3 +1,5 @@
+const SHAPES = Object.freeze({ L: 'L', O: 'O' });
+
 class Shape {
 	/**
 	 *
@@ -17,6 +19,44 @@ class Shape {
 		this.rotation_centre.x += position.x;
 		this.rotation_centre.y += position.y;
 		this.rotation = 0;
+	}
+
+	static generate_shape(shape) {
+		switch (shape) {
+			case 'L': {
+				return new Shape(
+					'L',
+					[
+						new Tile(new Vec2(0, -1), images[0]),
+						new Tile(new Vec2(0, 0), images[7]),
+						new Tile(new Vec2(1, 0), images[2]),
+						new Tile(new Vec2(2, 0), images[3])
+					],
+					new Vec2(1, 0),
+					new Vec2(0, -1)
+				);
+			}
+			case 'O': {
+				return new Shape(
+					'O',
+					[
+						new Tile(new Vec2(0, -1), images[0]),
+						new Tile(new Vec2(0, 0), images[7]),
+						new Tile(new Vec2(1, 0), images[2]),
+						new Tile(new Vec2(1, -1), images[3])
+					],
+					new Vec2(0.5, -0.5),
+					new Vec2(0, -1)
+				);
+			}
+		}
+	}
+
+	static generate_random_shape() {
+		const shapes_ids = Object.keys(SHAPES);
+		return Shape.generate_shape(
+			SHAPES[shapes_ids[Math.floor(Math.random() * shapes_ids.length)]]
+		);
 	}
 
 	rotate_clockwise() {
@@ -68,13 +108,100 @@ class Shape {
 	}
 
 	/**
+	 * Moves the shape one spot to the right
+	 */
+	move_right() {
+		this.tiles.forEach((t) => {
+			t.pos.x += 1;
+		});
+		this.rotation_centre.x += 1;
+	}
+
+	/**
+	 * Moves the shape one spot to the left
+	 */
+	move_left() {
+		this.tiles.forEach((t) => {
+			t.pos.x -= 1;
+		});
+		this.rotation_centre.x -= 1;
+	}
+
+	/**
 	 *
 	 * @returns {Array<Tile>} The tiles with the positions relative to the canvas
 	 */
 	place_down() {
-		return this.tiles.map((t) => {
-			t.x += this.position.x;
-			t.y += this.position.y;
+		return this.tiles;
+	}
+
+	width() {
+		let max_x = -Math.pow(2, 30);
+		let min_x = Math.pow(2, 30);
+		this.tiles.forEach((tile) => {
+			if (tile.pos.x > max_x) {
+				max_x = tile.pos.x;
+			}
+			if (tile.pos.x < min_x) {
+				min_x = tile.pos.x;
+			}
 		});
+		return max_x - min_x + 1;
+	}
+
+	left_x() {
+		let min_x = Math.pow(2, 30);
+		this.tiles.forEach((tile) => {
+			if (tile.pos.x < min_x) {
+				min_x = tile.pos.x;
+			}
+		});
+		return min_x;
+	}
+
+	right_x() {
+		let max_x = -Math.pow(2, 30);
+		this.tiles.forEach((tile) => {
+			if (tile.pos.x > max_x) {
+				max_x = tile.pos.x;
+			}
+		});
+		return max_x;
+	}
+
+	bottom_y() {
+		let max_y = -Math.pow(2, 30);
+		this.tiles.forEach((tile) => {
+			if (tile.pos.y > max_y) {
+				max_y = tile.pos.y;
+			}
+		});
+		return max_y;
+	}
+
+	/**
+	 *
+	 * @param {Vec2} canvas_size The dimensions of the canvas
+	 * @param {Array<Array<Tile>>} canvas_tiles The tiles placed on the canvas
+	 */
+	can_move_down(canvas_size, canvas_tiles) {
+		return this.bottom_y() < canvas_size.y - 1;
+	}
+
+	/**
+	 *
+	 * @param {Array<Array<Tile>>} canvas_tiles The tiles placed on the canvas
+	 */
+	can_move_left(canvas_tiles) {
+		return this.left_x() > 0;
+	}
+
+	/**
+	 *
+	 * @param {Vec2} canvas_size The dimensions of the canvas
+	 * @param {Array<Array<Tile>>} canvas_tiles The tiles placed on the canvas
+	 */
+	can_move_right(canvas_size, canvas_tiles) {
+		return this.right_x() < canvas_size.x - 1;
 	}
 }
